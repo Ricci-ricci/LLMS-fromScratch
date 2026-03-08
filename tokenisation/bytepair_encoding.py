7ffd838ff1dcded3e1256d31a5cc120ca0aaa545
Ricci-ricci <riccireese13@gmail.com>
2026-03-08 14:02:50 +0300

    input embedding features

--- tokenisation/bytepair_encoding.py
+++ tokenisation/bytepair_encoding.py
@@ -53,5 +53,25 @@ dataloader = create_dataloader_V1(
 )
 data_iter = iter(dataloader)
 first_batch = next(data_iter)
+second_batch = next(data_iter)
 
-print(first_batch)
+
+# let s try with a batch size greater than 1
+
+
+# positional embedings
+vocab_size = 50257
+output_dim = 256
+token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+max_len = 4
+dataloader = create_dataloader_V1(raw_text, batch_size=8, max_len=max_len, stride=4)
+data_iter = iter(dataloader)
+inputs, targets = next(data_iter)
+token_embeddings = token_embedding_layer(inputs)
+context_len = max_len
+pos_embedding_layer = torch.nn.Embedding(context_len, output_dim)
+pos_embeddings = pos_embedding_layer(torch.arange(context_len))
+input_embeddings = token_embeddings + pos_embeddings
+print(input_embeddings.shape)
+print(pos_embeddings.shape)
+print(token_embeddings.shape)
