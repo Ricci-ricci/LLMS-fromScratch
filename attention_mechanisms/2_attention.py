bb98bc5c3340d695a4119bdf6889309797196c4d
Ricci-ricci <riccireese13@gmail.com>
2026-03-09 14:59:46 +0300

    integration traingable weights

---
+++ attention_mechanisms/2_attention.py
@@ -0,0 +1,24 @@
+import torch
+
+# this is used for exemple purposes, in practice, you would use the tokeniser to convert your text into embeddings, and then use those embeddings as inputs to the attention mechanism.
+inputs = torch.tensor(
+    [
+        [0.43, 0.15, 0.89],  # Your(x^1)
+        [0.55, 0.87, 0.66],  # journey(x^2)
+        [0.57, 0.85, 0.64],  # starts(x^3)
+        [0.22, 0.58, 0.33],  # with(x^4)
+        [0.77, 0.25, 0.10],  # one(x^5)
+        [0.05, 0.80, 0.55],
+    ]  # step(x^6)
+)
+
+x_2 = inputs[1]
+d_in = inputs.shape[1]
+d_out = 2
+W_query = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+W_key = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+W_value = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+query_2 = x_2 @ W_query
+key_2 = inputs @ W_key
+value_2 = inputs @ W_value
+print(query_2)
